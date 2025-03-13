3d21ab98e4f8aa8633ee9a71d3f72e8ce54b6ea4
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-13 09:14:20 +0000

    add

--- srv/controller/MasterRegular.js
+++ srv/controller/MasterRegular.js
@@ -2,19 +2,37 @@ const express = require("express");
 const Regularizations = require("../model/Regularizations");
 const app = express();
 const getMethod =async (req,res)=>{
-const id = req.params.id
-const data = Regularizations.find();
-res.send("welcome "+id);
-}
+    try {
+        const employeecode = req?.params?.employeecode;
+        const getEmpData = await Regularizations.find({"employeecode":employeecode});
+        if(getEmpData.length > 0){
+                 res.status(200).json({message:"Success",Results:getEmpData}); 
+        }
+       } catch (error) {
+         res.status(500).json({ error: "Error Getting employee for Get", error: error.message });
+       }
+     };
+     
+
 const putMethod =async (req,res)=>{
-    const id = req.params.id
-    res.send("welcome "+id);
+   
+  try {
+    const employeecode = req?.params?.employeecode;
+    const data = req?.body;
+ const updatedEmp = await Regularizations.findOneAndUpdate({ employeecode: employeecode }, data, { new: true });
+res.status(201).json({
+         message: "Updated",
+         Emp: updatedEmp
+        });
+   
+  } catch (error) {
+    res.status(500).json({ error: "Error Posting Employe for Updated"});
+  }
     }
 const postMethod =async (req,res)=>{
     var data = req?.body;
     try {
       var created = Regularizations.create(data);
-  
   res.status(201).json({
            message: "Created",
            created:created
@@ -22,7 +40,7 @@ const postMethod =async (req,res)=>{
      
   
     } catch (error) {
-      res.status(500).json({ error: "Error Posting claims for Approval", error: error.message });
+      res.status(404).json({ error: "Error Posting Employee" });
     }
         }
 const deleteMethod =async (req,res)=>{
