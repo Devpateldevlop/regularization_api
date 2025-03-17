762b1088a0702e452930cc1746221fbc7227c531
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-17 12:51:10 +0000

    add

--- index.js
+++ index.js
@@ -76,6 +76,12 @@ app.post('/images',(req, res, next) => {
     res.status(201).send("Created")
 });
 
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end();
+});
 
 app.listen(PORT, () => {
 
