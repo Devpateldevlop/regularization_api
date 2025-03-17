3cdac0d11365f0e3d003cae015a2d9bca22bbcb0
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-17 13:11:54 +0000

    asdf

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ mongoose.connect('mongodb+srv://pdev5771:rxHFzG2xPEkkocvM@cluster0.bso1d.mongodb
     app.options('*', cors()); 
    
 
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.urlencoded({ extended: false }));
 // app.use(cors({orgin:'*'}));
 
