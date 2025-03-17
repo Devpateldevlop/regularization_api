4c7a375cf3210848f5c713aa0ae79f013a4556b2
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-17 05:57:17 +0000

    add multer

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var imgSchema = require('./srv/model/Image');
+var fs = require('fs');
+var path = require('path');
+
+
+var multer = require('multer');
 const cors = require('cors');
 require("dotenv").config();
 const { getMethod, postMethod, putMethod, deleteMethod } = require('./srv/controller/MasterRegular');
 const { getMethodCertificate, postMethodCertificate, putMethodCertificate, deleteMethodCertificate } = require('./srv/controller/Cetificate');
 const app = express();
 mongoose.connect('mongodb+srv://pdev5771:rxHFzG2xPEkkocvM@cluster0.bso1d.mongodb.net/test')
-    .then(() => console.log('MongoDB Connected...'))
+    .then((db) =>{
+        console.log('MongoDB Connected...');
+    })
     .catch((err) => console.log('MongoDB connection error: ' + err));
    
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({orgin:'*'}));
 const PORT = process.env.PORT || 5002;
@@ -26,6 +37,52 @@ app.get("/api/cetificate",getMethodCertificate);
 app.post("/api/cetificate",postMethodCertificate);
 app.put("/api/cetificate/:employeecode",putMethodCertificate);
 app.delete("/api/cetificate/:employeecode",deleteMethodCertificate);
+
+
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.get('/images', (req, res) => {
+    imgSchema.find({})
+    .then((data, err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.render('imagepage',{items: data})
+    })
+});
+app.post('/images', upload.single('image'), (req, res, next) => {
+
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    imgSchema.create(obj)
+    .then ((err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            res.redirect('/');
+        }
+    });
+});
+
+
 app.listen(PORT, () => {
 
     console.log(`Server is running on http://localhost:${PORT}`);
---
+++ srv/controller/Images.js
@@ -0,0 +1,54 @@
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var imgSchema = require('../model/Image');
+var fs = require('fs');
+var path = require('path');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.get('/images', (req, res) => {
+    imgSchema.find({})
+    .then((data, err)=>{
+        if(err){
+            console.log(err);
+        }
+        res.render('imagepage',{items: data})
+    })
+});
+app.post('/images', upload.single('image'), (req, res, next) => {
+
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    imgSchema.create(obj)
+    .then ((err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            res.redirect('/');
+        }
+    });
+});
+
