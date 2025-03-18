78fd1825d00269068abb34bc7c495a029420d099
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-18 05:37:09 +0000

    add

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var imgSchema = require('./srv/model/Image');
 var fs = require('fs');
 var path = require('path');
 var multer = require('multer');
