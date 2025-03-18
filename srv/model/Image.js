44ea89c01057a614b8b403f64136f515b77a6fd9
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-18 05:13:52 +0000

    delete all extra file not needed

--- srv/model/Image.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var imageSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    img:{type: String}
-});
-
-module.exports = mongoose.model('Image', imageSchema);
