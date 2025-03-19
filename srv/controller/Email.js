4d8b0e5a131c4ff8eca1a56eac3ded188ad28dfa
ahmed shaikh <ahmedshaikh@daffodilsinfo.com>
2025-03-19 12:00:01 +0000

    add email property

--- srv/controller/Email.js
+++ srv/controller/Email.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 const EmailPost =async (req, res) => {
     try {
-        const { senderEmail, senderFeedback,senderName } = req.body;
+        const { employeecode,lastName,firstName,EmailNotificationTo, FromDate,FromDateDayType,LeaveType,Remarks,ToDateDayType } = req.body;
 
         if (!senderEmail || !senderFeedback || !senderName) {
             return res.status(400).send("Sender email and feedback are required");
@@ -16,19 +16,25 @@ const EmailPost =async (req, res) => {
                 pass: 'kxcszpwbopestogz',
             },
         });
-    
        
+        var AllEmail = EmailNotificationTo.replace("|",",");
         const info = await transporter.sendMail({
           from: `"HRMate" <devpatel190703@gmail.com>`,
-          to: senderEmail,
-          subject: "Testing!",
-          text: `Dear ${senderName},\n\nThank you for taking the time to explore my HRMate and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.\n\nI’m truly grateful for your support and look forward to staying connected.\n\n If you’d like to revisit my portfolio or see updates, feel free to check it out.\n\nBest regards,\nHRMate`,
-          html: `
-              <p>Dear ${senderName},</p>
-              <p>Thank you for taking the time to explore my HRMate and share your valuable insights. Your feedback is not just appreciated – it’s invaluable, as it inspires me to strive for even greater heights.</p>
-              <p>I’m truly grateful for your support and look forward to staying connected.<br><br> If you’d like to revisit my portfolio or see updates, feel free to check it out."</p>
-              <p>Best regards,<br>HRMate</p>
-          `,
+          to: AllEmail,
+          subject: "HRMate Leave !",
+          text: `Dear HRMate Admin,\n Employee Code:${employeecode}\n First Name:${firstName}\n Last Name: ${lastName}\n Remark:${Remarks}\n Leave Type:${LeaveType}\n From Date:${LeaveType}\n From Date Day Type :${FromDateDayType}\n To Date Day Type :${ToDateDayType}\n\nBest regards,\nAhmed Shaikh`
+        ,   html: `
+            <p>Dear HRMate User,</p>
+            <p style='background-color:red'>Employee Code :${employeecode}</p>
+            <p>First Name :${firstName}</p>
+            <p>Last Name :${lastName}</p>
+            <p>Remark :${Remarks}</p><br>
+             <p>Leave Type :${LeaveType}</p>
+            <p>From Date :${FromDate}</p><br>
+             <p>From Date Day Type :${FromDateDayType}</p>
+            <p>To Date Day Type :${ToDateDayType}</p><br>
+            <p>Best regards,<br>Ahmed Shaikh</p>
+        `,
       });
         res.status(200).json({Message:"Thank-you email successfully sent"});
     } catch (error) {
@@ -48,11 +54,11 @@ const ReplyEmail=async (req,res)=>{
     });
     const infos = await transporter.sendMail({
         from: `"HRMate" <devpatel190703@gmail.com>`,
-        to: 'devpatel190703@gmail.com',
+        to: "devpatel190703@gmail.com",
         subject: `Feedback by ${senderName}`,
         text: `Dear HRMate Support,\n Date:${date}\n Name: ${senderName}\n Email:${senderEmail}\n Message:${senderFeedback}\n\nBest regards,\nAhmed Shaikh`
         ,   html: `
-            <p>Dear Ahmed Sir,</p>
+            <p>Dear User Sir,</p>
             <p style='background-color:red'>Date :${formattedDate}</p>
             <p>Name :${senderName}</p>
             <p>Email :${senderEmail}</p>
